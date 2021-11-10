d37b786222b27da5729f00a3422d33c9cf8190b1
flastest <pleasantbowel5yndrome@gmail.com>
2021-11-09 17:12:09 -0800

    wrote python

--- benchmarker.py
+++ benchmarker.py
@@ -1,3 +1,41 @@
 import bloom
-import time
+
+#this is my first time writing python code.
+
+
+
+data = range(966966)
+
+
+def test_hash_1():
+	list_of_hashes = bloom.hash_1(3, 256)
+
+	this_better_be_uniform = [0] * 256
+
+	for item in data:
+		for hash_ in list_of_hashes:
+			hash_val = hash_(item)
+			this_better_be_uniform[hash_val] += 1
+
+	print(this_better_be_uniform)
+
+
+def test_hash_2():
+	list_of_hashes = bloom.hash_2(3, 256)
+
+	this_better_be_uniform = [0] * 256
+
+	for item in data:
+		for hash_ in list_of_hashes:
+			hash_val = hash_(item)
+			this_better_be_uniform[hash_val] += 1
+
+	return this_better_be_uniform
+
+
+distribution = test_hash_2()
+
+print(distribution)
+print ("max of this distribution is ", max(distribution))
+print ("min of this distribution is ", min(distribution))
 
