da9b15e5aef92c77d9a1d8375ca9d2be8da4343d
flastest <pleasantbowel5yndrome@gmail.com>
2021-11-09 17:19:36 -0800

    I KNOW PYTHON

--- benchmarker.py
+++ benchmarker.py
@@ -4,38 +4,42 @@ import bloom
 
 
 
-data = range(966966)
-
 
 def test_hash_1():
-	list_of_hashes = bloom.hash_1(3, 256)
+		list_of_hashes = bloom.hash_1(3, 256)
 
-	this_better_be_uniform = [0] * 256
+		this_better_be_uniform = [0] * 256
 
-	for item in data:
-		for hash_ in list_of_hashes:
-			hash_val = hash_(item)
-			this_better_be_uniform[hash_val] += 1
+		for item in data:
+			for hash_ in list_of_hashes:
+				hash_val = hash_(item)
+				this_better_be_uniform[hash_val] += 1
 
-	print(this_better_be_uniform)
-
-
-def test_hash_2():
-	list_of_hashes = bloom.hash_2(3, 256)
-
-	this_better_be_uniform = [0] * 256
-
-	for item in data:
-		for hash_ in list_of_hashes:
-			hash_val = hash_(item)
-			this_better_be_uniform[hash_val] += 1
-
-	return this_better_be_uniform
+		print(this_better_be_uniform)
 
 
-distribution = test_hash_2()
+	def test_hash_2():
+		list_of_hashes = bloom.hash_2(3, 256)
 
-print(distribution)
-print ("max of this distribution is ", max(distribution))
-print ("min of this distribution is ", min(distribution))
+		this_better_be_uniform = [0] * 256
+
+		for item in data:
+			for hash_ in list_of_hashes:
+				hash_val = hash_(item)
+				this_better_be_uniform[hash_val] += 1
+
+		return this_better_be_uniform
+
+
+
+
+#I AM A PYTHON GOD
+def part_1():
+	data = range(966966)
+	
+	distribution = test_hash_2()
+
+	print(distribution)
+	print ("max of this distribution is ", max(distribution))
+	print ("min of this distribution is ", min(distribution))
 
